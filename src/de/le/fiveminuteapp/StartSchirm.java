ebb10c3a2db52dfd99d10f6aea3893c285685183
Philipp <philipp.ossler@gmail.com>
2013-09-18 23:18:58 +0200

    Bild eingefügt

--- src/de/le/fiveminuteapp/StartSchirm.java
+++ src/de/le/fiveminuteapp/StartSchirm.java
@@ -9,14 +9,13 @@ public class StartSchirm extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(de.le.fiveminuteapp.R.layout.activity_start_schirm);
+		setContentView(R.layout.activity_start_schirm);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater()
-				.inflate(de.le.fiveminuteapp.R.menu.start_schirm, menu);
+		getMenuInflater().inflate(R.menu.start_schirm, menu);
 		return true;
 	}
 
